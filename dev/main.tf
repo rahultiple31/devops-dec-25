1cde6155a55aaf3397aca6b2e8f24cb2cf2034c4
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2025-12-13 13:25:57 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,7 @@
-module "vpc" {
-  source = "git::https://github.com/rahultiple31/devops-dec-25.git//child-module/vpc?ref=main"
+# module "vpc" {
+#   source = "git::https://github.com/rahultiple31/devops-dec-25.git//child-module/vpc?ref=main"
 
-  name       = var.name
-  cidr_block = var.cidr_block
-  tags       = var.tags
-}
+#   name       = var.name
+#   cidr_block = var.cidr_block
+#   tags       = var.tags
+# }
