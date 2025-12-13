9ea79e71d75bda741c10f594ffc196e06c495509
rahultiple31 <rahultiple1@gmail.com>
2025-12-13 06:43:35 +0000

    terraform code

---
+++ prod/main.tf
@@ -0,0 +1,8 @@
+# module "vpc" {
+#   source = "./child-module/vpc"
+
+#   name            = var.name
+#   cidr_block      = var.cidr_block
+#   tags            = var.tags
+
+# }
---
+++ prod/terraform.tfvars
@@ -0,0 +1,7 @@
+name = "dev"
+cidr_block = "10.0.0.0/16"
+tags = {
+  Environment = "prod"
+  Project     = "itheroes"
+  Owner       = "rahul"
+}
