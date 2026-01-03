59916c5e6e47ecaedf8e05104c79dec713650db2
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2026-01-03 15:29:58 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Welcome to IT-Heroes Academy", app.getMessage());
     }
 }
