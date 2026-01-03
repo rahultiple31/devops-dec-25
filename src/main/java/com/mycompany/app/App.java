706b7f43ce1988cc97d6cec488562cc46c20e846
rahultiple31 <111124427+rahultiple31@users.noreply.github.com>
2026-01-03 15:43:03 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Welcome to IT-Heroes Academy";
+    private static final String MESSAGE = "Welcome to IT-Heroes Academy for Devops";
 
     public App() {}
 
